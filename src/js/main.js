084678974b1caee8ee4bdf45a238cfb55c99bd75
Max <thewildstar.m@gmail.com>
2023-01-29 13:37:55 +0300

    added section trainers

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,26 @@
 import * as functions from "./modules/functions.js";
+import Swiper, { Navigation, Scrollbar } from "swiper";
+
+const swiper = new Swiper(".trainers__slider", {
+	modules: [Navigation, Scrollbar],
+	slidesPerView: "auto",
+	spaceBetween: 40,
+	// slidesPerView: 1,
+	// spaceBetween: 20,
+	scrollbar: {
+		el: ".trainers__scrollbar",
+		draggable: true,
+	},
+	navigation: {
+		nextEl: ".trainers__arrow_next",
+		prevEl: ".trainers__arrow_prev",
+	},
+	// breakpoints: {
+	// 	575.98: {
+	// 		slidesPerView: 4,
+	// 		spaceBetween: 40,
+	// 	},
+	// },
+});
 
 functions.openBurgerMenu();
