1d4fe58e21014350280a76105bf65e67471baaf6
Max <thewildstar.m@gmail.com>
2023-01-31 22:48:36 +0300

    added tooltip in section benefit

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,6 @@
 import * as functions from "./modules/functions.js";
 import Swiper, { Navigation, Scrollbar } from "swiper";
+import { createPopper } from "@popperjs/core";
 
 const swiper = new Swiper(".trainers__slider", {
 	modules: [Navigation, Scrollbar],
@@ -15,5 +16,23 @@ const swiper = new Swiper(".trainers__slider", {
 	},
 });
 
+const tooltip = () => {
+	const tooltipBox = document.querySelector(".tooltip__ico");
+	const tooltip = document.querySelector(".tooltip__text");
+
+	createPopper(tooltipBox, tooltip, {
+		placement: "top-start",
+		modifiers: [
+			{
+				name: "offset",
+				options: {
+					offset: [0, 10],
+				},
+			},
+		],
+	});
+};
+
 functions.openBurgerMenu();
 functions.scrollToSections();
+tooltip();
